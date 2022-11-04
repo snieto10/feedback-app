44e11f2d75a6fcdc609aaff01d4c5b5d1f954d9c
Sergio Nieto <snieto10@gmail.com>
2022-11-04 14:44:10 -0400

    delete button added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import feedbackData from "./data/FeedbackData";
 import Card from "./components/shared/Card";
+import { FaWindows } from "react-icons/fa";
 
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
+
+  const deleteFeedback = (item) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      let feed = feedback.filter((f) => f.id !== item.id);
+      setFeedback(feed);
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
